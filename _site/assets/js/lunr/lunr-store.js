7e5ca913c1c7673ac892f1a391ee72a1af97f2e3
newwin01 <newwin0198@handong.ac.kr>
2023-02-26 16:51:10 +0900

    First Post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,13 @@
-var store = []
+var store = [{
+        "title": "[Jekyll] 블로그 포스팅하는 방법",
+        "excerpt":"테스트를 하고 있고 잘 안되는 것 같아 매우 힘들다.  ","categories": ["Blog"],
+        "tags": ["Blog","jekyll","Github","Git"],
+        "url": "/recent/my-first-post/",
+        "teaser": null
+      },{
+        "title": "[Jekyll] 블로그 포스팅하는 방법",
+        "excerpt":"테스트를 하고 있고 잘 안되는 것 같아 매우 힘들다.  ","categories": ["Blog"],
+        "tags": ["Blog","jekyll","Github","Git"],
+        "url": "/recent/Test/",
+        "teaser": null
+      }]
